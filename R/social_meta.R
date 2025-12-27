3e6b02cbe49389468896bf87db12ad8038d79e78
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-12-27 21:12:55 +0100

    added a few fields

--- R/social_meta.R
+++ R/social_meta.R
@@ -38,11 +38,20 @@ social_meta <- function(meta) {
     if (!is.null(meta$image_height))
       shiny::tags$meta(property="og:image:height", content=as.character(meta$image_height)),
 
+    if (!is.null(meta$image_type))
+      shiny::tags$meta(property="og:image:type", content=meta$image_type),
+
     shiny::tags$meta(name="twitter:card", content="summary_large_image"),
     shiny::tags$meta(name="twitter:title", content=meta$title),
     shiny::tags$meta(name="twitter:description", content=meta$description),
     shiny::tags$meta(name="twitter:image", content=meta$image),
     shiny::tags$meta(name="twitter:url", content=meta$url),
+
+    if (!is.null(meta$twitter_site))
+      shiny::tags$meta(name="twitter:site", content=meta$twitter_site),
+
+    if (!is.null(meta$twitter_creator))
+      shiny::tags$meta(name="twitter:creator", content=meta$twitter_creator),
 
     shiny::tags$title(meta$title)
   )
